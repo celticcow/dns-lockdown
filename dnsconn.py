be5758a2741f2dbe0ae542c966fc0e9db0963964
Celticcow <gregory.dunlap@gmail.com>
2020-12-08 13:03:19 -0600

    connection class

---
+++ dnsconn.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+"""
+class representing items from the splunk dump and connection parts
+"""
+
+class dnsconn(object):
+    """
+    origin | rule_name (direction) | src | dst | proto | service | action
+    """
+    #constructor
+    def __init__(self, origin="127.0.0.1", rule_name="null", src="127.0.0.1", dst="127.0.0.1", proto="1", service="0", action="null"):
+        self.origin = origin
+        self.rule_name = rule_name
+        self.src = src 
+        self.dst = dst
+        self.proto = proto
+        self.service = service
+        self.action = action
+
+    #modifiers
+    def set_origin(self, origin):
+        self.origin = origin
+    
+    def set_rule_name(self, rule_name):
+        self.rule_name = rule_name
+    
+    def set_src(self, src):
+        self.src = src
+    
+    def set_dst(self, dst):
+        self.dst = dst
+    
+    def set_proto(self, proto):
+        self.proto = proto
+
+    def set_service(self, service):
+        self.service = service
+    
+    def set_action(self, action):
+        self.action = action
+
+    #accessors
+    def get_origin(self):
+        return(self.origin)
+    
+    def get_rule_name(self):
+        return(self.rule_name)
+    
+    def get_src(self):
+        return(self.src)
+    
+    def get_dst(self):
+        return(self.dst)
+    
+    def get_proto(self):
+        return(self.proto)
+    
+    def get_service(self):
+        return(self.service)
+    
+    def get_action(self):
+        return(self.action)
+
+#end of class
--- dquery.py
+++ dquery.py
@@ -1,3 +1,32 @@
 #!/usr/bin/python3
 
+import requests
+import csv
+import sys
+import dnsconn
 
+
+"""
+Greg Dunlap / Celticcow
+
+edc2fl : 204.135.120.136/.137
+edc3fl : 204.135.120.134/.135
+wtc1fl : 172.31.124.18/.19
+wtc2fl : 172.31.104.14/.15
+"""
+
+def main():
+    print("start of main")
+
+    file1 = "file1.csv"
+
+    with open(file1) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            print(row[0])
+
+
+
+if __name__ == "__main__":
+    main()
+#end of program
